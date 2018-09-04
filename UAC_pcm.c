73be235ae54a020966442c9487dfb2709e7ece14
Kwang Myung, Jeon <lordet01@gmail.com>
2018-09-05 00:38:45 +0900

    UAC Record OK

--- UAC_pcm.c
+++ UAC_pcm.c
@@ -13,11 +13,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
+
+#define REC_LOCAL 0
 	      
 //Define Audio Parameters
 #define BUFF_LEN 4096
 #define NUM_CH 2
-#define SAMPLE_RATE 48000
 		  
 		  
 main (int argc, char *argv[])
@@ -26,29 +27,38 @@ main (int argc, char *argv[])
   int err;
   char *buffer;
 
-  //FILE *sig_FOut;
+  
 
 
   //Preparing sound capture device (physical)========
   snd_pcm_t *capture_handle;
   snd_pcm_hw_params_t *c_hw_params;
-	snd_pcm_format_t format = snd_pcm_FORMAT_S16_LE;
-	
-  if ((err = snd_pcm_open (&capture_handle, argv[1], snd_pcm_STREAM_CAPTURE, 0)) < 0) {
+	snd_pcm_t *playback_handle;
+	snd_pcm_hw_params_t *p_hw_params;
+	snd_pcm_format_t PCM_FORMAT = SND_PCM_FORMAT_S16_LE;
+  unsigned int SAMPLE_RATE = 48000;
+
+#if REC_LOCAL
+  FILE *sig_FOut;
+#endif
+
+  if ((err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
     fprintf (stderr, "cannot open audio capture device %s (%s)\n", 
              argv[1],
              snd_strerror (err));
     exit (1);
   }
   fprintf(stdout, "audio interface opened\n");
-/*
-  if ((sig_FOut = fopen(argv[3], "wb")) == NULL)
+
+#if REC_LOCAL
+  if ((sig_FOut = fopen(argv[2], "wb")) == NULL)
   {
   	printf("cannot open audio file for recording\n");
   	exit (1);
   } 
   fprintf(stdout, "audio file for recording opened\n");
-*/
+#endif
+
   if ((err = snd_pcm_hw_params_malloc (&c_hw_params)) < 0) {
     fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
              snd_strerror (err));
@@ -63,21 +73,21 @@ main (int argc, char *argv[])
   }
   fprintf(stdout, "c_hw_params initialized\n");
 	
-  if ((err = snd_pcm_hw_params_set_access (capture_handle, c_hw_params, snd_pcm_ACCESS_RW_INTERLEAVED)) < 0) {
+  if ((err = snd_pcm_hw_params_set_access (capture_handle, c_hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
     fprintf (stderr, "cannot set access type (%s)\n",
              snd_strerror (err));
     exit (1);
   }
   fprintf(stdout, "c_hw_params access setted\n");
 	
-  if ((err = snd_pcm_hw_params_set_format (capture_handle, c_hw_params, format)) < 0) {
+  if ((err = snd_pcm_hw_params_set_format (capture_handle, c_hw_params, PCM_FORMAT)) < 0) {
     fprintf (stderr, "cannot set sample format (%s)\n",
              snd_strerror (err));
     exit (1);
   }
   fprintf(stdout, "c_hw_params format setted\n");
 	
-  if ((err = snd_pcm_hw_params_set_SAMPLE_RATE_near (capture_handle, c_hw_params, &SAMPLE_RATE, 0)) < 0) {
+  if ((err = snd_pcm_hw_params_set_rate_near (capture_handle, c_hw_params, &SAMPLE_RATE, 0)) < 0) {
     fprintf (stderr, "cannot set sample SAMPLE_RATE (%s)\n",
              snd_strerror (err));
     exit (1);
@@ -109,10 +119,8 @@ main (int argc, char *argv[])
   fprintf(stdout, "audio capture interface prepared\n");
 //=====================================================================
 
-
+#if REC_LOCAL==0
   //Preparing sound capture device (UAC2)========
-		snd_pcm_t *playback_handle;
-		snd_pcm_hw_params_t *p_hw_params;
 	
 		if ((err = snd_pcm_open (&playback_handle, argv[2], SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
 			fprintf (stderr, "cannot open audio play device %s (%s)\n", 
@@ -139,13 +147,13 @@ main (int argc, char *argv[])
 			exit (1);
 		}
 	
-		if ((err = snd_pcm_hw_params_set_format (playback_handle, p_hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
+		if ((err = snd_pcm_hw_params_set_format (playback_handle, p_hw_params, PCM_FORMAT)) < 0) {
 			fprintf (stderr, "cannot set sample format (%s)\n",
 				 snd_strerror (err));
 			exit (1);
 		}
 	
-		if ((err = snd_pcm_hw_params_set_rate_near (playback_handle, p_hw_params, SAMPLE_RATE, 0)) < 0) {
+		if ((err = snd_pcm_hw_params_set_rate_near (playback_handle, p_hw_params, &SAMPLE_RATE, 0)) < 0) {
 			fprintf (stderr, "cannot set sample rate (%s)\n",
 				 snd_strerror (err));
 			exit (1);
@@ -171,46 +179,45 @@ main (int argc, char *argv[])
 			exit (1);
 		}
 //=====================================================================
+#endif
 
-
-  buffer = malloc(BUFF_LEN * (snd_pcm_format_width(format) >> 3) * NUM_CH);
+  buffer = malloc(BUFF_LEN * (snd_pcm_format_width(PCM_FORMAT) / 8) * NUM_CH);
   fprintf(stdout, "buffer allocated\n");
 
   while (1) 
   {
-    if ((err = snd_pcm_readi (capture_handle, buffer, BUFF_LEN)) != BUFF_LEN || )
+    if ((err = snd_pcm_readi (capture_handle, buffer, BUFF_LEN)) != BUFF_LEN)
 	  {
       fprintf (stderr, "read from audio interface failed (%s)\n",
                err, snd_strerror (err));
       exit (1);
     }
-		if ((err = snd_pcm_writei (playback_handle, buf, BUFF_LEN)) != BUFF_LEN) 
+#if REC_LOCAL==0
+		if ((err = snd_pcm_writei (playback_handle, buffer, BUFF_LEN)) != BUFF_LEN)
     {
 			fprintf (stderr, "write to audio interface failed (%s)\n",
 					     err, snd_strerror (err));
 				exit (1);
 		}
+#endif
 	
-    //fprintf(stdout, "read %d done\n", i);
-	  //fwrite(buffer, (BUFF_LEN*2), sizeof(short), sig_FOut);
+
+#if REC_LOCAL
+    fprintf(stdout, "read %d done\n", i);
+	  fwrite(buffer, (BUFF_LEN*2), sizeof(short), sig_FOut);
+#endif
     fprintf(stdout, "Sending pcm to Host...\r");
   }
 
-
   free(buffer);
-
-
-
-
-
-
   fprintf(stdout, "buffer freed\n");
 	
 
   snd_pcm_close (capture_handle);
-  fprintf(stdout, "audio interface closed\n");
+  fprintf(stdout, "audio capture interface closed\n");
 
-
+  snd_pcm_close (playback_handle);
+  fprintf(stdout, "audio playback interface closed\n");
 
 
 
